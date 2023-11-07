039775d2cc00f16ef826fcbb95df7569e23284cf
AquarioDeterminado <aquario.determinado@gmail.com>
2023-11-07 01:49:03 +0000

    All widgets initialized; still need event and spaces pages; Also need to finish datePikcer;

--- Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/MainPage.java
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/MainPage.java
@@ -1,6 +1,15 @@
 package pt.iade.ricardodiasjoaocoelho.projetosolar.views;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 
 import pt.iade.ricardodiasjoaocoelho.projetosolar.R;
 
@@ -10,5 +19,45 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainpage_events);
+
+        /*Widgets*/
+        ImageButton profileButton = findViewById(R.id.mainpage_profile_button);
+
+        Spinner datePicker = findViewById(R.id.mainpage_datePicker);
+
+        HorizontalScrollView eventsList = findViewById(R.id.mainpage_eventList);
+        LinearLayout spacesList = findViewById(R.id.mainpage_spaces_list);
+
+        /* --- Navigation --- */
+        Context context = this;
+        //MainPage -> Profile
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, Profile.class);
+                startActivity(intent);
+            }
+        });
+
+        //MainPage -> Event
+        eventsList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, Event.class);
+                startActivity(intent);
+            }
+        });
+
+        //MainPage -> Space
+        spacesList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, Space.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
 }
