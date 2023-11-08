6a6a51f63eb39f68fe743d55abff6a13fd7ad08f
AquarioDeterminado <aquario.determinado@gmail.com>
2023-11-08 08:23:15 +0000

    Event rsvp layput done; (still nedds strings)

--- Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/Event_RSVP.java
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/Event_RSVP.java
@@ -2,15 +2,20 @@ package pt.iade.ricardodiasjoaocoelho.projetosolar.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import pt.iade.ricardodiasjoaocoelho.projetosolar.R;
 
 public class Event_RSVP extends AppCompatActivity {
+    String eventID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_rsvp);
+
+        Intent intent = getIntent();
+        eventID = intent.getStringExtra("eventID");
     }
 }
--- Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/MainPage.java
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/MainPage.java
@@ -5,9 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 
@@ -26,6 +26,8 @@ public class MainPage extends AppCompatActivity {
         Spinner datePicker = findViewById(R.id.mainpage_datePicker);
 
         HorizontalScrollView eventsList = findViewById(R.id.mainpage_eventList);
+        Button eventsBttn = findViewById(R.id.mainpage_event_more_button);
+        Button spaceBttn = findViewById(R.id.mainpage_events_space_info_button_1);
         LinearLayout spacesList = findViewById(R.id.mainpage_spaces_list);
 
         /* --- Navigation --- */
@@ -40,15 +42,37 @@ public class MainPage extends AppCompatActivity {
         });
 
         //MainPage -> Event
-        eventsList.setOnClickListener(new View.OnClickListener() {
+        eventsBttn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, Event_RSVP.class);
+                String eventId = GetEventId();
+                intent.putExtra("eventID", eventId);
                 startActivity(intent);
+            }
+
+            String GetEventId()
+            {
+                View.OnClickListener something = this;
+                return "1";
             }
         });
 
         //MainPage -> Space
+        spaceBttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, Space_Info.class);
+                String spaceId = GetSpaceId();
+                intent.putExtra("spaceID", spaceId);
+                startActivity(intent);
+            }
+
+            String GetSpaceId()
+            {
+                return "1";
+            }
+        });
 
     }
 }
