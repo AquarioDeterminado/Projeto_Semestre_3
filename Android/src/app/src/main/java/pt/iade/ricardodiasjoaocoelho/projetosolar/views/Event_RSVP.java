aed76146ea8083cdcde4907a6b61f48277abc7ed
[Aquario Determinado] <aquario.determinado@gmail.com>
2023-11-09 08:53:17 +0000

    Events Functional; Still not able to send string to create SnackBar on previus Layout; Gona ask Stor Nathan and try not to cry about it;

--- Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/Event_RSVP.java
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/Event_RSVP.java
@@ -1,21 +1,78 @@
 package pt.iade.ricardodiasjoaocoelho.projetosolar.views;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.os.Bundle;
-
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import com.google.android.material.snackbar.Snackbar;
 import pt.iade.ricardodiasjoaocoelho.projetosolar.R;
 
 public class Event_RSVP extends AppCompatActivity {
     String eventID;
+
+    // Sends data back to MainPage
+    private OnDataReceivedListener onDataReceivedListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_rsvp);
 
+        /* --- Get eventID --- */
         Intent intent = getIntent();
         eventID = intent.getStringExtra("eventID");
+
+        /* --- Widgets --- */
+        ConstraintLayout background = findViewById(R.id.rsvp_background);
+
+        //TextView eventTitle = findViewById(R.id.rsvp_event_title);
+        //TextView eventDate = findViewById(R.id.rsvp_event_date);
+        //TextView eventDescrip = findViewById(R.id.rsvp_event_descrip);
+
+        Button acceptButton = findViewById(R.id.rsvp_accept_button);
+        Button notifyButton = findViewById(R.id.rsvp_notify_bttn);
+
+        /* --- Navigation --- */
+        //All go back to main page
+        notifyButton.setOnClickListener(
+                BackToMainPage(getString(R.string.rsvp_notify_message)));
+
+        acceptButton.setOnClickListener(
+                BackToMainPage(getString(R.string.rsvp_accept_message)));
+
+        background.setOnClickListener(
+                BackToMainPage(""));
+
+    }
+
+    private View.OnClickListener BackToMainPage (String message) {
+        return v -> {
+            Snackbar.make(v, message, Snackbar.LENGTH_LONG).show();
+            finish();
+            Log.d("RSVP", message);
+            if (!message.isEmpty())
+                sendDataBack(message);
+        };
+    }
+
+    public void SetOnDataReceivedListener (OnDataReceivedListener listener) {
+        this.onDataReceivedListener = listener;
+    }
+
+    private void sendDataBack (String data) {
+        if (onDataReceivedListener != null) {
+            onDataReceivedListener.onDataReceived(data);
+        }
+    }
+
+    public interface OnDataReceivedListener {
+        void onDataReceived(String data);
     }
 }
+
+
--- Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/MainPage.java
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/MainPage.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
@@ -11,9 +12,13 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 
+import com.google.android.material.snackbar.Snackbar;
+
 import pt.iade.ricardodiasjoaocoelho.projetosolar.R;
 
-public class MainPage extends AppCompatActivity {
+public class MainPage extends AppCompatActivity implements Event_RSVP.OnDataReceivedListener {
+
+    View layoutBackground;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,24 +26,25 @@ public class MainPage extends AppCompatActivity {
         setContentView(R.layout.mainpage_events);
 
         /*Widgets*/
+        layoutBackground = findViewById(R.id.mainpage_background);
+
         ImageButton profileButton = findViewById(R.id.mainpage_profile_button);
 
-        Spinner datePicker = findViewById(R.id.mainpage_datePicker);
+        //Spinner datePicker = findViewById(R.id.mainpage_datePicker);
 
-        HorizontalScrollView eventsList = findViewById(R.id.mainpage_eventList);
+        //HorizontalScrollView eventsList = findViewById(R.id.mainpage_eventList);
         Button eventsBttn = findViewById(R.id.mainpage_event_more_button);
         Button spaceBttn = findViewById(R.id.mainpage_events_space_info_button_1);
-        LinearLayout spacesList = findViewById(R.id.mainpage_spaces_list);
+        //LinearLayout spacesList = findViewById(R.id.mainpage_spaces_list);
 
         /* --- Navigation --- */
         Context context = this;
+        Event_RSVP.OnDataReceivedListener listener = this;
+
         //MainPage -> Profile
-        profileButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context, Profile.class);
-                startActivity(intent);
-            }
+        profileButton.setOnClickListener(v -> {
+            Intent intent = new Intent(context, Profile.class);
+            startActivity(intent);
         });
 
         //MainPage -> Event
@@ -46,14 +52,18 @@ public class MainPage extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, Event_RSVP.class);
+
                 String eventId = GetEventId();
                 intent.putExtra("eventID", eventId);
+
+                Event_RSVP event = new Event_RSVP();
+                event.SetOnDataReceivedListener(listener);
+
                 startActivity(intent);
             }
 
             String GetEventId()
             {
-                View.OnClickListener something = this;
                 return "1";
             }
         });
@@ -74,5 +84,11 @@ public class MainPage extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public void onDataReceived(String data) {
+        Snackbar.make(layoutBackground, data, Snackbar.LENGTH_LONG).show();
+        Log.d("RSVP", data);
     }
 }
