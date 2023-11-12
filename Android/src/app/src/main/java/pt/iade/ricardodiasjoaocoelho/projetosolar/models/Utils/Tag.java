9c82d55898f4f6d69e1b04d51cfd8ee8c1456cbc
[Aquario Determinado] <aquario.determinado@gmail.com>
2023-11-12 19:10:01 +0000

    I Don't remember :) XoXo, Go piss Girl; (Acho que não tá a funcionar btw)

---
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/models/Utils/Tag.java
@@ -0,0 +1,16 @@
+package pt.iade.ricardodiasjoaocoelho.projetosolar.models.Utils;
+
+public class Tag {
+    private String id;
+    private String descrip;
+
+    public Tag(String descrip)
+    {
+        this.descrip = descrip;
+    }
+
+    public String getDescrip()
+    {
+        return descrip;
+    }
+}
--- Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/Space_Info.java
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/Space_Info.java
@@ -3,14 +3,62 @@ package pt.iade.ricardodiasjoaocoelho.projetosolar.views;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
 
 import pt.iade.ricardodiasjoaocoelho.projetosolar.R;
+import pt.iade.ricardodiasjoaocoelho.projetosolar.models.Space.Space;
+import pt.iade.ricardodiasjoaocoelho.projetosolar.models.Utils.Tag;
 
 public class Space_Info extends AppCompatActivity {
+
+    private String spaceID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_space_info);
+
+        /* --- Get Space --- */
+        spaceID = getIntent().getStringExtra("spaceID");
+        Space space = new Space(spaceID);
+
+
+        /* --- Widgets --- */
+        TextView spaceTitle = findViewById(R.id.space_info_title);
+
+        ChipGroup spaceTagGroup = findViewById(R.id.space_info_tags);
+        List<Tag> tags = space.getTags();
+        ArrayList<View> chips = turnIntoChips(tags);
+        chips.forEach(chip -> {
+            spaceTagGroup.addView(chip);
+            styleChip((Chip) chip);
+        });
+    }
+
+    private ArrayList<View> turnIntoChips(List<Tag> tags) {
+        ArrayList<View> chips = new ArrayList<View>();
+        tags.forEach(tag -> {
+            Chip chip = new Chip(this);
+            chip.setText(tag.getDescrip());
+            chips.add(chip);
+        });
+        return chips;
+    }
+
+    private void styleChip (Chip chip){
+        RelativeLayout.LayoutParams layout = (RelativeLayout.LayoutParams) chip.getLayoutParams();
+        //layout.height = RelativeLayout.LayoutParams.WRAP_CONTENT;
+        layout.height = 50;
+        chip.setLayoutParams(layout);
+
     }
 }
