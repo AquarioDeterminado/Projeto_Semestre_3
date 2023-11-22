3960296ae71ef66ca48b00bb63b238ecf06b8ad7
[Aquario Determinado] <aquario.determinado@gmail.com>
2023-11-22 15:54:09 +0000

    Changed Cowork_Id to Fragment;

--- Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/Cowork_Id.java
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/Cowork_Id.java
@@ -1,16 +1,53 @@
 package pt.iade.ricardodiasjoaocoelho.projetosolar.views;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import pt.iade.ricardodiasjoaocoelho.projetosolar.R;
 
-public class Cowork_Id extends AppCompatActivity {
+public class Cowork_Id extends Fragment {
 
+    Cowork_Id() {
+        super(R.layout.activity_cowork_id);
+    }
+
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cowork_id);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+
+        ImageButton cameraBttn = view.findViewById(R.id.cowork_id_camera_button);
+
+        cameraBttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+
+                    Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                    intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE for bar codes
+
+                    startActivityForResult(intent, 0);
+
+                } catch (Exception e) {
+
+                    Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
+                    Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
+                    startActivity(marketIntent);
+
+                }
+            }
+        });
+
+        return view;
     }
 }
