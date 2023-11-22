22fecf7e038d10aa31c78046ac2f7fe983051c5c
[Aquario Determinado] <aquario.determinado@gmail.com>
2023-11-22 16:33:01 +0000

    Preciso de ir ver o outro codigo;

--- Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/Signup_User.java
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/Signup_User.java
@@ -2,15 +2,20 @@ package pt.iade.ricardodiasjoaocoelho.projetosolar.views;
 
 
 import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Message;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.contentcapture.ContentCaptureCondition;
 import android.widget.Button;
 
@@ -21,11 +26,7 @@ public class Signup_User extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signup_user_info);
-
-
-        Button finishBttn = findViewById(R.id.signup_finish_bttn);
-
+        setContentView(R.layout.signup_usr);
 
         /* --- CallBacks --- */
 
@@ -48,12 +49,36 @@ public class Signup_User extends AppCompatActivity {
         */
 
         /* --- Navigation --- */
+
+    }
+}
+
+
+class Signup_User_Info extends Fragment
+{
+    Signup_User_Info()
+    {
+        super(R.layout.signup_user_info);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =  super.onCreateView(inflater, container, savedInstanceState);
+
+        /* ---  Widgets --- */
+        Button nextbttn = view.findViewById(R.id.signup_next_bttn);
+
+        /* --- Navigation --- */
         nextbttn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                setContentView(R.layout.signup_user_password);
+                // já volto aqui
             }
         });
+
+        return view;
     }
+
 }
 
 class Signup_User_Password extends Fragment
@@ -63,22 +88,25 @@ class Signup_User_Password extends Fragment
         super(R.layout.signup_user_password);
     }
 
+    @Nullable
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
 
-        Context context = this;
+        assert view != null;
 
-        /* --- Widgetes --- */
+        /* ---  Widgets --- */
+        Button finifshbttn = view.findViewById(R.id.signup_finish_bttn);
 
-        Button nextbttn = findViewById(R.id.signup_next_bttn);
+        finifshbttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        return view;
     }
-}
-class Signup_User_Info extends Fragment
-{
-    Signup_User_Info()
-    {
-        super(R.layout.signup_user_info);
-    }
+
 }
 
