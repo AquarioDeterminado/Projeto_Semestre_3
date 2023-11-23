e9bc875f36722bc2fe6054132cb5af385e70bfb8
Ri787878 <114110536+Ri787878@users.noreply.github.com>
2023-11-23 23:37:27 +0000

    All files corrected to empty view activetys;

---
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/Company_Plan_Selelctor.java
@@ -0,0 +1,16 @@
+package pt.iade.ricardodiasjoaocoelho.projetosolar.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pt.iade.ricardodiasjoaocoelho.projetosolar.R;
+
+public class Company_Plan_Selelctor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plant_selelctor_location);
+    }
+}
