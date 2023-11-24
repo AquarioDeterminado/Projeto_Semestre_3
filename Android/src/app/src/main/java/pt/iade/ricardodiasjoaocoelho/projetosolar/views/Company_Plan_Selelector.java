4656b4347664c55b1314013ee94a8ba13fc1ffb0
Ri787878 <114110536+Ri787878@users.noreply.github.com>
2023-11-24 01:26:38 +0000

    creating a class Company and writing the CompanyListAdapter

--- Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/Company_Plan_Selelector.java
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/Company_Plan_Selelector.java
@@ -1,8 +1,10 @@
 package pt.iade.ricardodiasjoaocoelho.projetosolar.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 
 import pt.iade.ricardodiasjoaocoelho.projetosolar.R;
 
@@ -13,8 +15,15 @@ public class Company_Plan_Selelector extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan_selelector_location);
     }
+    /* ---  Widgets --- */
+    private View view;
+    RecyclerView compList = view.findViewById(R.id.plan_selector_company_list);
 
     /* --- Set Events --- */
     //Adapter
 
+
+}
+class CompanyListAdapter extends RecyclerView.Adapter<Company_Plan_Selelector.CompanyListAdapter.ViewHolder> {
+    private
 }
