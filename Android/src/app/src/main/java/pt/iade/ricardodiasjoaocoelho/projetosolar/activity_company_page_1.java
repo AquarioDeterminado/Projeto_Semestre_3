12113ed3cff0d2abe233148b1b831a437be7996e
Ri787878 <114110536+Ri787878@users.noreply.github.com>
2023-11-24 02:17:59 +0000

    a lot of progress my god

---
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/activity_company_page_1.java
@@ -0,0 +1,14 @@
+package pt.iade.ricardodiasjoaocoelho.projetosolar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_company_page_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company_page1);
+    }
+}
