d779f255f4c72f9426fc7f1db049ff8ea2082b12
Ri787878 <114110536+Ri787878@users.noreply.github.com>
2023-11-24 01:41:16 +0000

    class Company created

--- Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/controllers/Company.java
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/controllers/Company.java
@@ -2,4 +2,55 @@ package pt.iade.ricardodiasjoaocoelho.projetosolar.controllers;
 
 public class Company {
 
+        private String name;
+        private String address;
+        private String email;
+        private String phone;
+        private String website;
+        private String description;
+        private String logo;
+        private String[] tags;
+
+        public Company(String name, String address, String email, String phone, String website, String description, String logo, String[] tags) {
+            this.name = name;
+            this.address = address;
+            this.email = email;
+            this.phone = phone;
+            this.website = website;
+            this.description = description;
+            this.logo = logo;
+            this.tags = tags;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public String[] getTags() {
+            return tags;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+        public String getWebsite() {
+            return website;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public String getLogo() {
+            return logo;
+        }
 }
