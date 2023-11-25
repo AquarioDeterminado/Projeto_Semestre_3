207fbc2aae555f38ee6c7b43f081c67f0d89c5ad
Ri787878 <114110536+Ri787878@users.noreply.github.com>
2023-11-25 17:51:21 +0000

    settings string ofprofile updated

--- Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/Profile.java
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/Profile.java
@@ -10,6 +10,8 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        
     }
 
 }
