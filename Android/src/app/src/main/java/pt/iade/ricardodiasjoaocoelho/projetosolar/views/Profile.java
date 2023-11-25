f7b53a646ccf25d188c3b3c3e79eaf85140bbc3c
Ri787878 <114110536+Ri787878@users.noreply.github.com>
2023-11-25 18:07:28 +0000

    1st version of button from profile to settings page (not tested) (Most likelly not working)

--- Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/Profile.java
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/Profile.java
@@ -1,7 +1,12 @@
 package pt.iade.ricardodiasjoaocoelho.projetosolar.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import pt.iade.ricardodiasjoaocoelho.projetosolar.R;
 
 public class Profile extends AppCompatActivity {
@@ -11,7 +16,19 @@ public class Profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        
+        // Find the settings button by its ID
+        Button settingsButton = findViewById(R.id.profile_settings_button);
+
+        // Set a click listener for the settings button
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Start the SettingsActivity when the button is clicked
+                Intent myintent;
+                myintent = new Intent(Profile.this, Settings_Page.class);
+                startActivity(myintent);
+            }
+        });
     }
 
 }
---
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/Settings_Page.java
@@ -0,0 +1,13 @@
+package pt.iade.ricardodiasjoaocoelho.projetosolar.views;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import pt.iade.ricardodiasjoaocoelho.projetosolar.R;
+public class Settings_Page extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_page);
+
+
+
+}
