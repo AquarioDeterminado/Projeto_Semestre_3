e7092cca7c241de590f9e62c8cf077be65056cf2
[Aquario Determinado] <aquario.determinado@gmail.com>
2023-12-02 16:28:00 +0000

    Added sql connection;

--- Prototype/SQL/Projeto Solar - Table Creation.sql
+++ Prototype/SQL/Projeto Solar - Table Creation.sql
@@ -1,17 +1,21 @@
 create table entity (
+	#ID's
 	ent_id int not null auto_increment,
     ent_name VARCHAR(60) not null,
+    
+    #Contacts
+	ent_phone VARCHAR(15) not null,
+    ent_email VARCHAR(40) not null,
 	
+    #Account Info
     ent_creation_date datetime not null, 
 	PRIMARY KEY (ent_id)
 );
 
-
 create table individual (
 	#ID
 	ind_id int not null auto_increment, 
-    
-    ind_cmp_id int,
+    ind_ent_id int not null,
     
     #LogIn Info
     ind_email VARCHAR(40) not null,
@@ -20,21 +24,23 @@ create table individual (
     #Personal Info
 	ind_name VARCHAR(60) not null,
     ind_bdate date not null,
-    ind_phone VARCHAR(15) not null,
 
     #Account Info
-    ind_creation_date datetime not null, 
+    ind_account_creation_date datetime not null, 
     ind_active bool not null default 1,
     
      CONSTRAINT ind_pk PRIMARY KEY (ind_id, ind_email)
 );
 
+create table company (
+	cmp_id int not null auto_increment,
+
+	cmp_location VARCHAR(40),
+    PRIMARY KEY (cmp_id)
+);
+
 create table client (
 	cli_id int not null auto_increment,
-    
-    cli_cmp_id int,
-    cli_ind_id int,
-    
     PRIMARY KEY (cli_id)
 );
 
@@ -43,7 +49,7 @@ create table entities_subscribed (
     ens_sub_id int not null
 );
 
-create table events (
+create table event (
 	#ID
 	evt_id int not null auto_increment,
     
@@ -72,27 +78,27 @@ create table events (
 create table facility (
 	fac_id int not null auto_increment,
     fac_name VARCHAR(40) not null,
-    PRIMARY KEY (etg_id)
+    PRIMARY KEY (fac_id)
 );
 
-create table events_facilities_list (
+create table events_facilities (
 	evf_evt_id int not null,
     evf_fac_id int not null
 );
 
-create table events_type(
+create table event_type(
 	ety_id int not null auto_increment,
     ety_type VARCHAR(20),
 	PRIMARY KEY (ety_id)
 );
 
-create table cowork_spaces (
+create table cowork_space (
 	#ID
 	spc_id int not null auto_increment,
     
     #Basic Info
     spc_name VARCHAR(40) not null,
-    spc_locations_list VARCHAR(80) not null,
+    spc_locations VARCHAR(80) not null,
     
     #Stats
     spc_max_capacity int not null,
@@ -106,21 +112,12 @@ create table cowork_spaces (
     PRIMARY KEY (spc_id)
 );
 
-create table cowork_space_facilities_list (
+create table cowork_spaces_facilities (
 	cof_fac_id int not null,
     cof_spc_id int not null
 );
 
-create table company (
-	cmp_id int not null auto_increment,
-    
-    cmp_name VARCHAR(20) not null,
-    cmp_phone VARCHAR(15) not null,
-    cmp_email VARCHAR(40) not null,
-    PRIMARY KEY (cmp_id)
-);
-
-create table company_employees (
+create table company_employee (
 	coe_cmp_id int not null,
     coe_ent_id int not null
 );
@@ -137,17 +134,26 @@ create table subscription (
     PRIMARY KEY (sub_id)
 );
 
-
-create table subscriptions_facilities_list (
+create table subscriptions_facilities (
 	suf_fac_id int not null,
     suf_sub_id int not null
 );
 
-create table users_subscriptions (
-	uss_cli_id int not null,
-    uss_sub_id int not null,
+create table subscriptions (
+	sus_id int not null auto_increment,
+	sus_cli_id int not null,
+    sus_sub_id int not null,
     
-    uss_init_date date not null
+    sus_init_date date not null,
+    sus_is_active boolean not null default 1,
+    PRIMARY KEY (sus_id)
+);
+
+create table subscribed (
+	sud_sus_id int not null,
+    sud_ent_id int not null,
+    
+    sud_is_active bool not null default 1
 );
 
 create table rsvp (
@@ -162,21 +168,32 @@ create table rsvp (
 create table workstations_space (
 	wsp_id int not null auto_increment,
     wsp_spc_id int not null,
+    
+    #SVG of the floor plan
+    wsp_floor_plan VARCHAR(400),
     PRIMARY KEY (wsp_id)
 );
 
 create table workstations_group (
 	wgr_id int not null auto_increment,
     wgr_wsp_id int not null,
-    PRIMARY KEY (wsg_id)
+    PRIMARY KEY (wgr_id)
 );
 
 create table workstation (
 	wks_id int not null auto_increment,
-	wks_wrg_id int not null
+	wks_wrg_id int not null,
+    
+    PRIMARY KEY (wks_id)
 );
 
-create table workstations_uses (
+create table workstation_uses (
+	wus_ind_id int not null,
+    wus_wks_id int not null,
+    wus_init_time datetime not null,
+    wus_final_time datetime
+);
+
 	wus_ind_id int not null,
     wus_wks_id int not null,
     wus_init_time datetime not null,
