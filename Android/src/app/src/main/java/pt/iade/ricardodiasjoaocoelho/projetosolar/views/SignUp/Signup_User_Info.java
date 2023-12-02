37d83fa70751bf63fa2f9cb0d86f64fcc3b09f10
[Aquario Determinado] <aquario.determinado@gmail.com>
2023-12-02 17:06:52 +0000

    Register now verifies input;

--- Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/SignUp/Signup_User_Info.java
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/SignUp/Signup_User_Info.java
@@ -5,11 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import pt.iade.ricardodiasjoaocoelho.projetosolar.R;
 
@@ -26,17 +30,37 @@ public class Signup_User_Info extends Fragment {
         /* ---  Widgets --- */
         Button nextbttn = view.findViewById(R.id.signup_next_bttn);
 
+
+        ArrayList<TextView> textViews = new ArrayList<>();
+        textViews.add(view.findViewById(R.id.signup_usr_name));
+        textViews.add(view.findViewById(R.id.signup_usr_email));
+        textViews.add(view.findViewById(R.id.signup_usr_phone));
+        textViews.add(view.findViewById(R.id.signup_usr_bdate));
+
         /* --- Navigation --- */
         nextbttn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                FragmentManager fragmentManager = getParentFragmentManager();
-                fragmentManager.beginTransaction()
-                        .replace(R.id.signup_usr_frame, new Signup_User_Password())
-                        .commit();
+                AtomicBoolean allFilled = new AtomicBoolean(true);
+                textViews.forEach(textView -> {
+                    if(textView.getText().toString().isEmpty()){
+                        textView.setError("This field is required!");
+                        allFilled.set(false);
+                    }
+                });
+                if(allFilled.get()){
+                    nextFragment();
+                }
             }
         });
 
         return view;
+    }
+
+    private void nextFragment(){
+        FragmentManager fragmentManager = getParentFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.signup_usr_frame, new Signup_User_Password())
+                .commit();
     }
 
 }
