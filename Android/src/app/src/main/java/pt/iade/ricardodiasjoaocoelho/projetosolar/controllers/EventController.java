94ad22510165b577935925d94f4e2317005880b4
[Aquario Determinado] <aquario.determinado@gmail.com>
2024-01-05 18:20:41 +0000

    Event Views some what finished;

--- Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/controllers/EventController.java
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/controllers/EventController.java
@@ -8,9 +8,30 @@ public class EventController {
 
     public static ArrayList<Event> getCurrentEvents(User_Info user)
     {
-        ArrayList<Event> eventArrayList = new ArrayList<>();
-        eventArrayList.add(new Event("1"));
-        eventArrayList.add(new Event("2"));
-        return eventArrayList;
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    try {
+                        // This is a brand new object and must be a INSERT in the database.
+                        WebRequest req = new WebRequest(new URL(
+                                WebRequest.LOCALHOST + "/saveNote/"));
+                        Log.i("WebRequest", "Post made to API:" + newNote.toString());
+                        String response = req.performPostRequest(newNote);
+
+                        // Get the new ID from the server's response.
+                        NoteItem respItem = new Gson().fromJson(response, NoteItem.class);
+                        returnNote.response(respItem);
+
+                    } catch (Exception e) {
+                        Log.e("TodoItem", e.toString());
+                        Toast.makeText(null, "Web request failed: " + e.toString(),
+                                Toast.LENGTH_LONG).show();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
