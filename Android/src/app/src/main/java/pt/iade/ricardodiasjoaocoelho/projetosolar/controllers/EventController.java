0ef29dbc23b13e4bf5d59da834cb36a76880098f
[Aquario Determinado] <aquario.determinado@gmail.com>
2024-01-06 21:48:35 +0000

    Doesn´t send the event info;

--- Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/controllers/EventController.java
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/controllers/EventController.java
@@ -3,6 +3,8 @@ package pt.iade.ricardodiasjoaocoelho.projetosolar.controllers;
 import android.util.Log;
 
 import com.google.gson.Gson;
+
+import java.io.Serializable;
 import java.net.URL;
 import pt.iade.ricardodiasjoaocoelho.projetosolar.models.Event.Event;
 import pt.iade.ricardodiasjoaocoelho.projetosolar.models.User.User_Info;
@@ -10,8 +12,7 @@ import pt.iade.ricardodiasjoaocoelho.projetosolar.utils.WebRequest;
 
 public class EventController {
 
-    public static void getCurrentEvents(User_Info user, ReturnEvents returnEvents)
-    {
+    public static void getCurrentEvents(int userId, ReturnEvents returnEvents) {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -19,26 +20,27 @@ public class EventController {
                     try {
                         // This is a brand new object and must be a INSERT in the database.
                         WebRequest req = new WebRequest(new URL(
-                                WebRequest.LOCALHOST + "/Solar/events/getAvailableEvents"));
-                        Log.i("WebRequest", "Post made to API: Events for user");
-                        String response = req.performPostRequest(user.getId());
+                                WebRequest.LOCALHOST + "/Solar/events" + "/getAvailableEvents"));
+                        Log.i("WebRequest", "Get made to API: Events for user");
+                        Id id = new Id(userId);
+                        String response = req.performPostRequest(id);
 
                         // Get the new ID from the server's response.
                         Event[] availableEvents = new Gson().fromJson(response, Event[].class);
                         returnEvents.response(availableEvents);
 
                     } catch (Exception e) {
-                        Log.e("Even", e.toString());
+                        Log.e("AvailableEvents", e.toString());
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         });
+        thread.start();
     }
 
-    public void getUserEvents(User_Info user, ReturnEvents returnEvents)
-    {
+    public static void getUserEvents(int userId, ReturnEvents returnEvents) {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -48,7 +50,7 @@ public class EventController {
                         WebRequest req = new WebRequest(new URL(
                                 WebRequest.LOCALHOST + "/Solar/events/getRSVP"));
                         Log.i("WebRequest", "Post made to API: Events RSVP'ed by user");
-                        String response = req.performPostRequest(user.getId());
+                        String response = req.performPostRequest(userId);
 
                         // Get the new ID from the server's response.
                         Event[] availableEvents = new Gson().fromJson(response, Event[].class);
@@ -62,14 +64,22 @@ public class EventController {
                 }
             }
         });
+        thread.start();
     }
 
-    interface ReturnEvents {
+    public interface ReturnEvents {
         void response(Event[] events);
     }
 
     interface ReturnEvent {
         void response(Event event);
     }
+}
 
+class Id implements Serializable {
+    int id;
+
+    public Id(int id) {
+        this.id = id;
+    }
 }
---
+++ Android/src/app/src/main/java/pt/iade/ricardodiasjoaocoelho/projetosolar/views/adapters/EventListAdapter.java
@@ -0,0 +1,79 @@
+package pt.iade.ricardodiasjoaocoelho.projetosolar.views.adapters;
+
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.recyclerview.widget.RecyclerView;
+
+import pt.iade.ricardodiasjoaocoelho.projetosolar.R;
+import pt.iade.ricardodiasjoaocoelho.projetosolar.models.Event.Event;
+import pt.iade.ricardodiasjoaocoelho.projetosolar.views.Event_RSVP;
+
+public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.ViewHolder> {
+    private Event[] eventDataSet;
+    private ActivityResultLauncher<Intent> eventLauncher;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView eventDate;
+
+        private final ImageView eventImage;
+        private final TextView eventTitle;
+        private final Button eventRSVP;
+        public ViewHolder(View view) {
+            super(view);
+            eventDate = view.findViewById(R.id.mainpage_event_row_item_name1);
+            eventImage = view.findViewById(R.id.mainpage_event_row_item_img);
+            eventTitle = view.findViewById(R.id.mainpage_event_row_item_name2);
+            eventRSVP = view.findViewById(R.id.mainpage_event_row_item_more_button);
+        }
+
+    }
+    public EventListAdapter(Event[] eventDataSet) {
+        this.eventDataSet = eventDataSet;
+    }
+
+    @Override
+    public EventListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.mainpage_event_row_item, parent, false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(EventListAdapter.ViewHolder holder, int position) {
+        /* --- Set Widgets --- */
+        holder.eventTitle.setText(eventDataSet[position].getTitle());
+        holder.eventDate.setText(eventDataSet[position].getStartTime().toString());
+        //holder.eventImage.setImageResource(eventDataSet[position].getImage());
+
+        holder.eventRSVP.setText((R.string.mainpage_events_show_me_more_bttn));
+        /* --- Navigation --- */
+        holder.eventRSVP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext() , Event_RSVP.class);
+                int eventId = eventDataSet[position].getId();
+                intent.putExtra("eventID", eventId);
+                eventLauncher.launch(intent);
+            }
+        });
+    }
+
+    public void setEventLauncher(ActivityResultLauncher<Intent> eventLauncher) {
+        this.eventLauncher = eventLauncher;
+    }
+
+    @Override
+    public int getItemCount() {
+        return eventDataSet.length;
+    }
+}
